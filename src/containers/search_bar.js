8b845528f60b0c00528d7f9cce21acf535d28672
Nikolas  Haug <nikolashaug@Nikolass-MacBook-Air.local>
2018-11-08 16:45:32 -0600

    add axios and middleware for redux

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
         this.state = { term: '' };
 
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -16,9 +20,17 @@ export default class SearchBar extends Component {
         )
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+        
+        // we need to fetch weather data here
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' });
+    }
+
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input 
                     placeholder="get a five-day forecast in your favorite cities"
                     className="form-control"
@@ -32,3 +44,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
